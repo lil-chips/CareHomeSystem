fffe4491dd93b791c479af12f7e8a53bd094a503
Edward Chang <skw10615@gmail.com>
2025-10-08 17:00:17 +1100

    Create CareHomeDatabase

---
+++ src/main/java/edu/rmit/cosc1295/carehome/CareHomeDatabase.java
@@ -0,0 +1,11 @@
+package edu.rmit.cosc1295.carehome;
+
+import java.sql.*;
+
+/**
+ *
+ */
+public class CareHomeDatabase {
+
+    private static final String DB_URL = "jdbc:sqlite:care_home.db"; // file name
+}
