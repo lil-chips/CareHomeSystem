6b2149318c5fa30f74b3a0a3b87a03da435cb82b
Edward Chang <skw10615@gmail.com>
2025-10-08 13:17:28 +1100

    Fixed up some issues

--- src/main/java/edu/rmit/cosc1295/carehome/Main.java
+++ src/main/java/edu/rmit/cosc1295/carehome/Main.java
@@ -12,12 +12,6 @@ public class Main {
             home = new CareHome();
             System.out.println("No data found. Creating a new one.");
 
-            Manager mgr = new Manager("M1", "Manager One", "pw");
-            home.addStaff(mgr, mgr);
-            Nurse nurse = new Nurse("N1", "Nurse Lily", "pw");
-            home.addStaff(mgr, nurse);
-            home.assignShift(mgr, nurse, new Shift("MONDAY", "08:00-16:00"));
-
 
         }
 
