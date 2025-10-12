935fa5da541006d6a7a5d99a0b1b254aea8fe37d
Edward Chang <skw10615@gmail.com>
2025-10-12 12:42:49 +1100

    Added test duplicate staff ID

--- src/test/java/CareHomeTest.java
+++ src/test/java/CareHomeTest.java
@@ -1,8 +1,14 @@
 import org.junit.jupiter.api.*;
-
 import static org.junit.jupiter.api.Assertions.*;
+import edu.rmit.cosc1295.carehome.*;
 
 public class CareHomeTest {
 
+    // Duplicate staff ID throw error
+    @Test
+    @DisplayName("addStaff() should throw error if same ID is added twice")
+    void addStaff_duplicateId_throwsError() {
+
+    }
 
 }
