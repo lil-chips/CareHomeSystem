bcf8e848e5c745ff3c0c98f2ced24f56a3ca4bdb
Edward Chang <skw10615@gmail.com>
2025-10-12 12:51:17 +1100

    Added more codes in test duplicate staff ID

--- src/test/java/CareHomeTest.java
+++ src/test/java/CareHomeTest.java
@@ -9,6 +9,17 @@ public class CareHomeTest {
     @DisplayName("addStaff() should throw error if same ID is added twice")
     void addStaff_duplicateId_throwsError() {
 
+        CareHome c = new CareHome();
+        Manager manager = new Manager("manager1", "Edward", "0722");
+        Nurse nurse = new Nurse("nurse1", "Qin", "1234");
+        // Manager add new nurse
+        assertDoesNotThrow(() -> c.addStaff(manager, nurse));
+
+        // Add another nurse with same ID
+        Nurse dup = new Nurse("nurse1", "Mona", "6666");
+        assertThrows(IllegalArgumentException.class,
+                () -> c.addStaff(manager, dup),
+                "Same ID should not be allowed");
     }
 
 }
