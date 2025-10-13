fbb9988788d4a1a36cea1b6aa994f3aeb586b63e
Edward Chang <skw10615@gmail.com>
2025-10-13 16:40:08 +1100

    Added more codes in dashboardController

--- src/main/java/edu/rmit/cosc1295/ui/DashboardController.java
+++ src/main/java/edu/rmit/cosc1295/ui/DashboardController.java
@@ -1,7 +1,10 @@
 package edu.rmit.cosc1295.ui;
 
 import javafx.fxml.FXML;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
@@ -12,6 +15,13 @@ public class DashboardController {
 
     @FXML
     void onLogout(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText("You have logged out successfully.");
+        alert.showAndWait();
 
+        // Close this window
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
     }
 }
