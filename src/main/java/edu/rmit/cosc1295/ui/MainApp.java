85f8636500933d4b4e2af932201afb7115d1be6f
Edward Chang <skw10615@gmail.com>
2025-10-13 15:48:31 +1100

    Added FXMLLoader in MainApp.java

--- src/main/java/edu/rmit/cosc1295/ui/MainApp.java
+++ src/main/java/edu/rmit/cosc1295/ui/MainApp.java
@@ -2,6 +2,8 @@ package edu.rmit.cosc1295.ui;
 
 import edu.rmit.cosc1295.carehome.CareHome;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.*;
 
 /**
@@ -11,5 +13,9 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception{
         CareHome app = new CareHome();
+
+        // Load the FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/rmit/cosc1295/ui/login.fxml"));
+        Scene scene = new Scene(loader.load(), 520, 360);
     }
 }
