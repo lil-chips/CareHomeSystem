d2a727864148de947281a9ab20e763562a265e47
Edward Chang <skw10615@gmail.com>
2025-10-13 18:30:01 +1100

    Try to build staff list controller.java

--- src/main/java/edu/rmit/cosc1295/ui/StaffListController.java
+++ src/main/java/edu/rmit/cosc1295/ui/StaffListController.java
@@ -1,4 +1,25 @@
 package edu.rmit.cosc1295.ui;
 
+import javafx.fxml.FXML;
+import edu.rmit.cosc1295.carehome.CareHome;
+import edu.rmit.cosc1295.carehome.Staff;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+
+
 public class StaffListController {
+
+    @FXML
+    private TableView<Staff> staffTable;
+
+    @FXML
+    private TableColumn<Staff, String> idCol;
+
+    @FXML
+    private TableColumn<Staff, String> nameCol;
+
+    @FXML
+    private TableColumn<Staff, String> roleCol;
+
+    private CareHome model;
 }
