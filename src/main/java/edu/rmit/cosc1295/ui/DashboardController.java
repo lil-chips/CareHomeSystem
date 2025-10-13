9986efc812375d613b668062fd7772c03b95a135
Edward Chang <skw10615@gmail.com>
2025-10-13 16:34:38 +1100

    Added more codes in dashboardController

--- src/main/java/edu/rmit/cosc1295/ui/DashboardController.java
+++ src/main/java/edu/rmit/cosc1295/ui/DashboardController.java
@@ -1,4 +1,17 @@
 package edu.rmit.cosc1295.ui;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class DashboardController {
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    void onLogout(ActionEvent event) {
+
+    }
 }
