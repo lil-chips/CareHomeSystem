e9eb9abfd7e889975c2aafa4382fd051930dfc07
Edward Chang <skw10615@gmail.com>
2025-10-13 17:52:29 +1100

    Modify onLogout in dashboardController.java

--- src/main/java/edu/rmit/cosc1295/ui/DashboardController.java
+++ src/main/java/edu/rmit/cosc1295/ui/DashboardController.java
@@ -2,7 +2,10 @@ package edu.rmit.cosc1295.ui;
 
 import edu.rmit.cosc1295.carehome.CareHome;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import java.awt.*;
@@ -29,14 +32,13 @@ public class DashboardController {
 
     @FXML
     void onLogout(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/rmit/cosc1295/ui/login.fxml"));
+            Scene loginScene = new Scene(loader.load(), 600, 400);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        } catch (Exception e) {
 
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setHeaderText(null);
-        alert.setContentText("You have logged out successfully.");
-        alert.showAndWait();
+        }
 
-        // Close this window
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.close();
     }
 }
