a76b59f8e9fb7454dc2cd682dcb03b4a98f3e2a8
Edward Chang <skw10615@gmail.com>
2025-10-13 15:22:13 +1100

    Try to build up JavaFX

--- src/main/java/edu/rmit/cosc1295/ui/LoginController.java
+++ src/main/java/edu/rmit/cosc1295/ui/LoginController.java
@@ -1,4 +1,34 @@
 package edu.rmit.cosc1295.ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+
+/**
+ * Controller for login.fxml, handles login UI actions.
+ */
 public class LoginController {
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private ChoiceBox<String> roleChoice;
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
+    }
+
+
 }
