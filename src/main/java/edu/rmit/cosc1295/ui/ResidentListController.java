cd4f4dce4859a6b11efe8324b297f693696f030f
Edward Chang <skw10615@gmail.com>
2025-10-14 20:41:04 +1100

    Added setLoggedInStaff and onBack

--- src/main/java/edu/rmit/cosc1295/ui/ResidentListController.java
+++ src/main/java/edu/rmit/cosc1295/ui/ResidentListController.java
@@ -3,9 +3,14 @@ package edu.rmit.cosc1295.ui;
 import edu.rmit.cosc1295.carehome.CareHome;
 import edu.rmit.cosc1295.carehome.Resident;
 import edu.rmit.cosc1295.carehome.Staff;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
 public class ResidentListController {
     @FXML
@@ -22,5 +27,36 @@ public class ResidentListController {
 
     private CareHome model;
     private Staff loggedInStaff;
+
+
+    public void setLoggedInStaff(Staff staff) {
+        this.loggedInStaff = staff;
+    }
+
+    /**
+     * Return to dashboard when user clicks "Back".
+     * @param event The button click event
+     */
+
+    @FXML
+    void onBack(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/rmit/cosc1295/ui/Dashboard.fxml"));
+            Scene scene = new Scene(loader.load(), 600, 400);
+
+            DashboardController controller = loader.getController();
+            controller.setModel(model);
+            controller.setLoggedInStaff(loggedInStaff);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("CareHome - Dashboard");
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
 }
