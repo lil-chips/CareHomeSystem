7841451f87962d37e4c6fd6031f2fb94a5a6dd6e
Edward Chang <skw10615@gmail.com>
2025-10-14 19:40:39 +1100

    Starts building up moveresidentcontroller

--- src/main/java/edu/rmit/cosc1295/ui/MoveResidentController.java
+++ src/main/java/edu/rmit/cosc1295/ui/MoveResidentController.java
@@ -1,4 +1,24 @@
 package edu.rmit.cosc1295.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+import java.awt.*;
+
+/**
+ * Controller for MoveResident.fxml
+ * Allows nurses to move a resident from one bed to another.
+ */
+
 public class MoveResidentController {
+
+    @FXML
+    private ChoiceBox<String> residentChoice;
+
+    @FXML
+    private ChoiceBox<Integer> bedChoice;
+
+    private CareHome model;
+    private Staff loggedInStaff;
+
 }
