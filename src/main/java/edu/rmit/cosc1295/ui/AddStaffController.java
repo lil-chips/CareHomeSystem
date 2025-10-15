e9a81a9df95fc4c5231198d38e7a80bbfcb61b79
Edward Chang <skw10615@gmail.com>
2025-10-15 18:35:15 +1100

    Added addStaffController

--- src/main/java/edu/rmit/cosc1295/ui/AddStaffController.java
+++ src/main/java/edu/rmit/cosc1295/ui/AddStaffController.java
@@ -1,4 +1,33 @@
 package edu.rmit.cosc1295.ui;
 
+import edu.rmit.cosc1295.carehome.CareHome;
+import edu.rmit.cosc1295.carehome.Staff;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class AddStaffController {
+
+    @FXML private TextField idField;
+    @FXML private TextField nameField;
+    @FXML private TextField passwordField;
+    @FXML private ChoiceBox<String> roleChoice;
+
+    private CareHome model;
+    private Staff loggedInStaff;
+
+    @FXML
+    public void initialize() {
+        // Provide role options
+        roleChoice.getItems().addAll("Manager", "Doctor", "Nurse");
+        roleChoice.setValue("Nurse");
+    }
+
+    public void setModel(CareHome model) {
+        this.model = model;
+    }
+
+    public void setLoggedInStaff(Staff staff) {
+        this.loggedInStaff = staff;
+    }
 }
