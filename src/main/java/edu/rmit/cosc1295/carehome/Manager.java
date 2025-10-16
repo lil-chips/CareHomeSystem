9b19daa066080046d0ed18138f5db1a0ba194d85
Edward Chang <skw10615@gmail.com>
2025-10-16 18:29:53 +1100

    Add comments and deleted unused code

--- src/main/java/edu/rmit/cosc1295/carehome/Manager.java
+++ src/main/java/edu/rmit/cosc1295/carehome/Manager.java
@@ -4,11 +4,12 @@ package edu.rmit.cosc1295.carehome;
  * Manager class is the system administrator
  * The manager can create new staff accounts and reset passwords.
  */
+
 public class Manager extends Staff {
 
-    // Inherits from Staff
     /**
      * Construct them with id, name and password
+     * Inherits from Staff
      * @param id ID of manager
      * @param name Name of manager
      * @param password Password of manager
@@ -18,23 +19,24 @@ public class Manager extends Staff {
         super(id, name, password);
     }
 
-
     /**
-     * Modify or set up staff's password
-     *
+     * Allows a manager to update the password of a staff member.
      * @param staff The staff whose password will be updated
      * @param newPassword The new password
      */
 
     public void modifyStaffPassword(Staff staff, String newPassword) {
+        // Staff can't be null
         if (staff == null) {
-            throw new IllegalArgumentException("Staff cannot be null");
+            throw new IllegalArgumentException("Staff can't be null");
         }
+        // Password can't be null or blank
         if (newPassword == null || newPassword.isBlank()) {
             throw new IllegalArgumentException("New password can't be null or blank");
         }
 
         try {
+            // Update password
             staff.setPassword(newPassword);
             System.out.println("Manager " + name + " reset password for " + staff.getName());
 
@@ -43,33 +45,10 @@ public class Manager extends Staff {
                     + " (ID: " + staff.getId() + ")";
             CareHome.createLog(showlog);
         } catch (Exception e) {
+            // Log the failure and rethrow the exception
             CareHome.createLog("Password reset failed: " + staff.getName() + " due to: "
                     + e.getMessage());
             throw e;
         }
-    }
-
-    /**
-     * Add a new staff member to the system.
-     * Ask CareHome to handle saving and checking.
-     *
-     * @param careHome the CareHome that manages all data
-     * @param newStaff the staff you want to add
-     */
-
-    public void addStaff(CareHome careHome, Staff newStaff) {
-        careHome.addStaff(this, newStaff);
-    }
-
-    /**
-     * Add a new resident and put them into a bed.
-     * Ask CareHome to do the actual work.
-     *
-     * @param careHome the CareHome that manages all data
-     * @param newResident the resident you want to add
-     * @param bedId the bed number to assign
-     */
-    public void addResident(CareHome careHome, Resident newResident, int bedId) {
-        careHome.addResident(this, newResident, bedId);
     }
 }
